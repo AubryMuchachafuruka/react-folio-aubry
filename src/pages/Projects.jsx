49c452d9dae1ae1aa1607d744237aa63373e45ed
Munyaradzi Muchafuruka <aubreyubuntu@gmail.com>
2025-09-26 11:04:22 +0200

    first

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <>
+     <h1>Projects</h1>
+     <ul>
+      <li>
+      <strong style={{ textAlign: 'center'}} >Student Dashboard Project:</strong>
+        <a href="https://aubrymuchachafuruka.github.io/munya-portifolio-livedemo-sites/" target="_blank"
+        >
+          <div style={{ marginBottom: '10px' }}> <u>Live Demo! </u></div> </a>
+       
+         A responsive dashboard built with <em>HTML, CSS, and JavaScript</em>, featuring clean design, interactive elements, and a user-friendly layout. Perfect for academic tracking and portfolio presentation.</li></ul>
+
+    </>
+  );
+} 
+   
