2dbc04b1df0c30a5c27fd4ebb8676b545e8c86ea
Munyaradzi Muchafuruka <aubreyubuntu@gmail.com>
2025-09-26 12:26:29 +0200

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className="px-2 py-2">
         <div className="flex items-center justify-center text-center px-7 py-7 bg-gray-100 rounded-lg">
           <p className="w-full">
-          © 2025 Muchafuruka – in React <a href="https://github.com/kaanayyildiz" className="font-medium underline" >React</a>
+          © 2025 Muchafuruka – in <a href="https://github.com/AubryMuchachafuruka" className="font-medium underline" >React</a>
           </p>
         </div>
       </div>
